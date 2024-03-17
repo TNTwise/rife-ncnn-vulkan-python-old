95dcc68bb39109a835aca180a9501612824dcb3f
TNTwise <tntwise3@gmail.com>
2024-03-16 21:35:56 -0500

    update submodule

--- rife_ncnn_vulkan_python/rife_ncnn_vulkan.py
+++ rife_ncnn_vulkan_python/rife_ncnn_vulkan.py
@@ -20,10 +20,7 @@ import numpy as np
 if __package__ is None:
     import rife_ncnn_vulkan_wrapper as wrapped
 else:
-    try:
-        wrapped = importlib.import_module(f"{__package__}.rife_ncnn_vulkan_wrapper")
-    except ImportError:
-        wrapped = importlib.import_module(f"{__package__}_TAS.rife_ncnn_vulkan_wrapper")
+    wrapped = importlib.import_module(f"{__package__}.rife_ncnn_vulkan_wrapper")
 
 
 class Rife:
